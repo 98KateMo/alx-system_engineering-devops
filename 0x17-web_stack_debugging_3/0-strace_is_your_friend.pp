c75cafe50e33555c4a9eef563778154f7f4ac3d3
98KateMo <kr.molefe98@gmail.com>
2023-12-14 21:17:56 +0000

    files debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
- bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# Fix error 500 server error of a LAMP stack
+exec { 'sed_replace':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/bin:/usr/bin',
+  onlyif  => 'grep -q phpp /var/www/html/wp-settings.php',
 }
